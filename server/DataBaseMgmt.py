5e7dbe07291a76dfae23dd3f00aefa7969c750f7
Clif Wolfe <clifwolfe1@gmail.com>
2019-04-02 14:48:00 -0400

    Fixed insert not working and a bug caused by a decorator

--- server/DataBaseMgmt.py
+++ server/DataBaseMgmt.py
@@ -10,29 +10,29 @@ class DBManager:
     def __init__(self):
         self.db_name = "form_data.db"
         self.conn = None
-        self.create_db()
+        if not os.path.exists(self.db_name):
+            # this has to go outside of create_db because decorator already creates db
+            self.create_db()
 
     def create_connection(self):
         self.conn = sqlite3.connect(self.db_name)
-        return self.conn
 
-    def connect(self, func):
-        try:
-            self.conn.execute('SELECT name FROM sqlite_temp_master WHERE type="table";')
-        except (AttributeError, sqlite3.ProgrammingError):
-            self.conn = sqlite3.connect(self.db_name)
+    def connect(func):
 
-        def inner_func(*args, **kwargs):
-            return func(*args, **kwargs)
-        ret_val = inner_func
-        self.conn.commit()
-        return ret_val
+        def inner_func(self, *args, **kwargs):
+            try:
+                self.conn.execute('SELECT name FROM sqlite_temp_master WHERE type="table";')
+            except (AttributeError, sqlite3.ProgrammingError):
+                self.create_connection()
+            ret_val = func(self, *args, **kwargs)
+            self.conn.commit()
+            return ret_val
+
+        return inner_func
 
     @connect
     def create_db(self):
         """Creates the database if it does not already exist"""
-        if os.path.exists(self.db_name):
-            return
 
         c = self.conn.cursor()
         c.execute("""CREATE TABLE FormData
@@ -46,16 +46,18 @@ class DBManager:
                      FirstName text, 
                      LastName text,
                      MNumber text,
-                     MacAddress text)""")
+                     MacAddress text);""")
+        c.close()
+
 
     @connect
     def insert_record(self, table_name, *args):
-        if len(args) > 4 or len(args) < 4:
+        if len(*args) > 4 or len(*args) < 4:
             raise IncorrectArgs
 
         c = self.conn.cursor()
         c.execute(f"""INSERT INTO 
                   {table_name}(FirstName, LastName, MNumber, MacAddress)
-                  VALUES(?,?,?,?)""", *args)
-
+                  VALUES(?,?,?,?);""", *args)
+        c.close()
 
